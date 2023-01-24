315702e32a897e2fb0f4830ceffa09b2bb91a207
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-24 16:56:54 +0100

    setup functions in JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-console.log("Witam serdecznie! Jeśli spodziewałeś się cudów... no coż. Ale jest to już na GitHub'ie");
-
-const button = document.querySelector(".js-buttonChangeBackgroundColor");
-const trollButton = document.querySelector(".js-buttonJustForTrolling");
-const container = document.querySelector(".container");
-const exchanageApp = document.querySelector("#js-inputValue")
-
-button.addEventListener("click", () => {
-    container.classList.toggle("container--modified");
-});
-trollButton.addEventListener("click", () => {
-    trollButton.classList.toggle("button--modified")
-
-    trollButton.innerHTML = trollButton.classList.contains("button--modified") ? "Prosiłem nie KLIKAJ ?!" : "Nie klikaj w ten przycisk!"
-});
+{
+    const welcome = () => {
+        console.log("Witam serdecznie! Jeśli spodziewałeś się cudów... no coż. Ale jest to już na GitHub'ie");
+    }
+    
+    const toggleBackgroundColor = () => {
+        const container = document.querySelector(".container");
+        container.classList.toggle("container--modified");
+    }
+    
+    const trollButtonAction = () => {
+        const trollButton = document.querySelector(".js-buttonJustForTrolling");
+        trollButton.classList.toggle("button--modified")
+        trollButton.innerHTML = trollButton.classList.contains("button--modified") ? "Prosiłem nie KLIKAJ ?!" : "Nie klikaj w ten przycisk!"
+    }
+    
+    const init = () => {
+        welcome()
+        const button = document.querySelector(".js-buttonChangeBackgroundColor");
+        const trollButton = document.querySelector(".js-buttonJustForTrolling");
+        button.addEventListener("click", toggleBackgroundColor);
+        trollButton.addEventListener("click", trollButtonAction);      
+    }
+    
+    init()
+}
